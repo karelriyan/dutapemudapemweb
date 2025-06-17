b3284d77e35ec04baae1a98e8015cb526bfe684e
Karel <karelriyan@gmail.com>
2025-06-17 18:08:50 +0700

    page faq tahap 2

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,210 +1,43 @@
 @extends('layouts.app')
-@section('title', 'Berita')
+@section('title', 'FAQ - Frequently Asked Questions')
 @section('content')
 
     <section class="blog section" id="blog">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="section-title-judul-berita">
-                        <h2>SELEKSI PPAN & PPAP TINGKAT
-                            KOTA TANGGERANG SELATAN
-                            TAHUN 2025</h2>
+                    <div class="section-title-judul-faq">
+                        <h2>Frequently Asked Questions (FAQ)</h2><br>
                     </div>
-                    <h3 style="margin-bottom: 30px; font-size: medium;">25/06/2024, 08:45 WIB</h3>
-                    <img src="img/slider.png" alt="Berita" class="img-fluid"
-                        style="width: 100%; height: auto; margin-bottom: 20px;">
+                    <h3 style="margin-bottom: 30px; font-size: medium;">Apa itu platform ini?</h3>
                     <p style="text-align: justify; font-size: medium; margin-bottom: 50px;">
-                        KOMPAS.com - Pemerintah Indonesia melalui Badan Siber dan Sandi Negara (BSSN) akhirnya
-                        mengungkap penyebab gangguan Pusat Data Nasional (PDN) yang terjadi sejak Kamis (20/6/2024)
-                        pekan lalu.<br><br>
-
-                        Kepala BSSN Hinsa Siburian mengatakan bahwa gangguan tersebut disebabkan oleh serangan
-                        ransowmare bernama brain cipher.<br><br>
-
-                        "Perlu kami sampaikan bahwa insiden pusat data sementara ini adalah serangan siber dalam bentuk
-                        ransomware, dengan nama Brain Cipher ransomware," kata Hinsa dalam konferensi pers yang
-                        disiarkan di YouTube Kemkominfo TV. <br><br>
-
-                        Ransomware sendiri adalah program jahat (malware) yang dapat mengunci data di komputer dengan
-                        enkripsi. Pelaku penyebar ransomware lalu akan memeras korban dengan meminta tebusan dalam
-                        jumlah tertentu untuk membuka kunci tersebut. <br><br>
-
-                        Menurut Direktur Network dan IT Solutions Telkom Herlan Wirjanako, ransomware Brain Cipher ini
-                        mengunci data PDN dan meminta tebusan sebesar 8 juta dollar AS (sekitar Rp 131,2 miliar).<br><br>
-
-                        "Jadi memang di dark web (situs gelap) itu ada jalan dan kita ikuti, mereka (pihak yang menyebar
-                        ransomware) minta tebusan ada 8 juta dollar AS. Demikian," kata Herlan. <br><br>
-
-                        Investigasi menyeluruh<br><br>
-
-                        Kini, BSSN bersama Kemkominfo, Cyber Crime Kepolisian RI (Polri), dan KSO Telkomsigma masih
-                        terus berproses mengupayakan investigasi secara menyeluruh.<br><br>
-
-                        "Masih mengupayakan investigasi secara menyeluruh pada bukti-bukti forensik yang didapat, dengan
-                        segala keterbatasan barang bukti. Karena kondisinya barang bukti terenkripsi. Ini juga harus
-                        kami pecahkan," lanjut Hinsa.<br><br>
-
-                        Selain investigasi, tim gabungan BSSN masih berupaya mengatasi dampak dari serangan tersebut,
-                        termasuk memulihkan data yang terkunci serta layanan publik terdampak.<br><br>
-
-                        “Upaya-upaya ke sana sudah kami rumuskan dan kami diskusikan tadi, sehingga diharapkan bisa
-                        dengan cepat masalah ini, kejadian ini bisa diatasi dengan baik,” kata Hinsa.<br><br>
-
-                        Hinsa juga melaporkan kemajuan upaya yang dilakukan pemerintah per Senin (24/6/2024) pagi
-                        ini.<br><br>
-
-                        Ia mengatakan bahwa layanan imigrasi yang terdampak sudah beroperasi dengan normal, mulai dari
-                        layanan visa dan izin tinggal, layanan paspor, hingga layanan manajemen dokumen
-                        keimigrasian.<br><br>
-
-                        "Ini sudah berjalan, walaupun nantinya tetap akan dilakukan evaluasi-evaluasi berikutnya," ujar
-                        Hinsa sebagaimana dikutip KompasTekno dari YouTube Kemkominfo TV.<br><br>
-
-                        Diberitakan sebelumnya, gangguan sistem pada PDN Kemenkominfo berdampak terhadap layanan
-                        keimigrasian di seluruh Indonesia pada Kamis (20/6/2024).<br><br>
-
-                        Menkominfo Budi Arie Setiadi mengatakan, sejumlah layanan publik terdampak akibat kerusakan
-                        sistem pada PDN.<br><br>
+                        Platform ini digunakan untuk pendaftaran lomba, manajemen peserta, dan penilaian lomba secara
+                        online.
                     </p>
-                </div>
-            </div>
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-title-berita">
-                        <h2>Berita Lainnya</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="img/slider2.png" alt="#" />
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                <div class="date">22 Aug, 2020</div>
-                                <h2>
-                                    <a href="blog-single.html">We have annnocuced our new product.</a>
-                                </h2>
-                                <p class="text">
-                                    Lorem ipsum dolor a sit ameti, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt sed do incididunt sed.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Blog -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="img/slider3.png" alt="#" />
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                <div class="date">15 Jul, 2020</div>
-                                <h2>
-                                    <a href="blog-single.html">Top five way for solving teeth problems.</a>
-                                </h2>
-                                <p class="text">
-                                    Lorem ipsum dolor a sit ameti, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt sed do incididunt sed.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Blog -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="img/slider.png" alt="#" />
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                <div class="date">05 Jan, 2020</div>
-                                <h2>
-                                    <a href="blog-single.html">We provide highly business soliutions.</a>
-                                </h2>
-                                <p class="text">
-                                    Lorem ipsum dolor a sit ameti, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt sed do incididunt sed.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Blog -->
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="img/slider3.png" alt="#" />
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                <div class="date">15 Jul, 2020</div>
-                                <h2>
-                                    <a href="blog-single.html">Top five way for solving teeth problems.</a>
-                                </h2>
-                                <p class="text">
-                                    Lorem ipsum dolor a sit ameti, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt sed do incididunt sed.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Blog -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="img/slider3.png" alt="#" />
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                <div class="date">15 Jul, 2020</div>
-                                <h2>
-                                    <a href="blog-single.html">Top five way for solving teeth problems.</a>
-                                </h2>
-                                <p class="text">
-                                    Lorem ipsum dolor a sit ameti, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt sed do incididunt sed.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Blog -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="img/slider3.png" alt="#" />
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                <div class="date">15 Jul, 2020</div>
-                                <h2>
-                                    <a href="blog-single.html">Top five way for solving teeth problems.</a>
-                                </h2>
-                                <p class="text">
-                                    Lorem ipsum dolor a sit ameti, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt sed do incididunt sed.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Blog -->
-                </div>
+                    <h3 style="margin-bottom: 30px; font-size: medium;">Bagaimana cara mendaftar akun?</h3>
+                    <p style="text-align: justify; font-size: medium; margin-bottom: 50px;">
+                        Klik tombol "Daftar" di halaman utama, lalu isi formulir dengan data yang benar dan lengkap.
+                    </p>
 
+                    <h3 style="margin-bottom: 30px; font-size: medium;">Apakah saya bisa mengubah data setelah mendaftar?
+                    </h3>
+                    <p style="text-align: justify; font-size: medium; margin-bottom: 50px;">
+                        Ya, kamu bisa mengedit data melalui halaman dashboard selama periode pendaftaran masih dibuka.
+                    </p>
+
+                    <h3 style="margin-bottom: 30px; font-size: medium;">Bagaimana cara mengikuti lomba?</h3>
+                    <p style="text-align: justify; font-size: medium; margin-bottom: 50px;">
+                        Setelah login, pilih lomba yang tersedia, isi formulir pendaftaran, dan unggah berkas yang diminta.
+                    </p>
+
+                    <h3 style="margin-bottom: 30px; font-size: medium;">Siapa yang bisa saya hubungi jika mengalami kendala?
+                    </h3>
+                    <p style="text-align: justify; font-size: medium; margin-bottom: 50px;">
+                        Kamu bisa menghubungi tim panitia melalui kontak yang tersedia di halaman “Hubungi Kami”.
+                    </p>
+
+                </div>
             </div>
         </div>
     </section>
