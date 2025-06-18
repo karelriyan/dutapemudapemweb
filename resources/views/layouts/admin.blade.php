d60bc40781ae06993bb9da9cf6f5dbdd0b52e9f2
HammasHaryaSena <162069949+HammasHaryaSena@users.noreply.github.com>
2025-06-18 16:26:33 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -193,7 +193,7 @@
                         <img src="{{ asset('img/user.png') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Syahrial Hipdi</a>
+                        <a href="#" class="d-block">Karel Riyan</a>
                     </div>
                 </div>
 
